1c27444000979cb248127bc0ab2c20613329050b
taoshen182 <1227602328@qq.com>
2018-01-21 22:41:39 +0800

    服务注册成功

--- person/src/main/java/com/example/demo/PersonApplication.java
+++ person/src/main/java/com/example/demo/PersonApplication.java
@@ -1,22 +1,15 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@RestController
+@EnableEurekaClient
 public class PersonApplication {
-    @RequestMapping("/index")
-    public String index() {
-        return "in person idnex";
-    }
+
 
     public static void main(String[] args) {
-//        SpringApplication.run(PersonApplication.class, args);
-        new SpringApplicationBuilder(PersonApplication.class).web(true).run(args);
+        SpringApplication.run(PersonApplication.class, args);
     }
 }
--- person/src/main/java/com/example/demo/PersonController.java
+++ person/src/main/java/com/example/demo/PersonController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author : wangtao
  * @date : 2018/1/19 13:51
  */
-
+@RestController
 public class PersonController {
-
+    @RequestMapping("/index")
+    public String index() {
+        return "in person idnex";
+    }
 }
